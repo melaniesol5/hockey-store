81d24b2f7010f8ec96f1ae48d8c9f5eb2b0f2569
melaniesol5 <melaniealarcon97@hotmail.com>
2021-08-02 23:31:05 -0300

    add cart widget and item list container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from "./components/NavBar";
+import ItemListContainer from './components/ItemListContainer';
+
 function App() {
   return (
     <div className="App">
       <header>
         <NavBar/>
       </header>
+    <ItemListContainer greeting="Bienvenido!"/>
     </div>
   );
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,5 @@
+export default function ItemListContainer(props){
+    return(
+       <p>{props.greeting}</p> 
+    )
+}
